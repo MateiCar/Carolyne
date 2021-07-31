ad9df13780d551a2d1f20c7d59f12f02f03c1234
MateiCar <88235443+MateiCar@users.noreply.github.com>
2021-07-31 15:23:20 +0300

    Create Rectangle.cs

---
+++ Rectangle.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace SerializeDemo
+
+{
+
+[Serializable]
+
+public class Rectangle:Shape
+
+{
+
+public double Height { get; set; }
+
+public double Width { get; set; }
+
+// Rectangle Area = Height * Width
+
+public override double Area { get { return Height * Width; } }
+
+}
+
+}
